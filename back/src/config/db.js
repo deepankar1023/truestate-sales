01198c953486054f6a9e53aef89bf01bd51705ba
deepankar1023 <161783724+deepankar1023@users.noreply.github.com>
2025-12-09 13:00:16 +0530

    security patch

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://deepankarmishra77060_db_user:K5x8Aw32ecfC5J40@cluster0.chnljpz.mongodb.net/")
+    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb")
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
     console.error(`Error: ${error.message}`)
