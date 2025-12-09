932ee3fcc6ac75f05e8a81750f903e09b82fde63
deepankar1023 <161783724+deepankar1023@users.noreply.github.com>
2025-12-09 14:18:38 +0530

    backend connection fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      "/api": "http://localhost:5000"
+      "/api": {
+        target: process.env.VITE_API_URL, 
+        changeOrigin: true,
+        secure: false,
+      }
     }
   }
-})
+});
